a32a7fe010562822125a6e5636f38868aa6298a6
MM-Morsi <69103168+MM-Morsi@users.noreply.github.com>
2021-11-24 14:04:03 +0200

    Add code

---
+++ index.js
@@ -0,0 +1,15 @@
+// let image = document.getElementById("image");
+// let imageDiv = document.getElementById("imageDiv")
+
+// window.addEventListener("resize", () => {
+//   if (window.innerWidth < 700) {
+//     image.src = "../images/image-header-mobile.jpg";
+//     imageDiv.style.backgroundImage = "url()";
+//   } else if (window.innerWidth >= 700) {
+//     image.src = "";
+//     imageDiv.style.backgroundImage = "url(images/image-header-desktop.jpg)";
+//     // + 'linear-gradient(#000000, #ffffff))';
+//   }
+// });
+
+// // https://stackoverflow.com/questions/68104913/background-image-doesnt-appear-when-using-media-query
